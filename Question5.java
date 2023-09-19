bd16c84ebef11a8f379069a74cd80b55f0e5162f
Sudeep Neupane <122254713+SudeepNeupane42@users.noreply.github.com>
2023-09-19 07:21:24 +0545

    Add files via upload

--- Question5.java
+++ Question5.java
@@ -1,25 +1,38 @@
-
-package labsheet6;
+package labsheet7;
 
 public class Question5 {
-     public static <T> void swap(T[] arr,int first,int second){
-        T temp;
-        temp=  arr[first];
-        arr[first]=arr[second];
-        arr[second]=temp;
-    
-}
+
     public static void main(String[] args) {
-        Integer[] arrr={1,2,3,4,5};
-        
-        System.out.println("before swap array:");
-        for(int i:arrr){
-            System.out.println(i);
-        }
-        swap(arrr, 0, 1);
-         System.out.println("before swap array:");
-        for(int i:arrr){
-            System.out.println(i);
-        }
+        int a = 20;
+        int b = 21;
+        int c = Math.abs(a);
+        System.out.println(c);
+        c = Math.max(a, b);
+        System.out.println(c);
+        c = Math.min(a, b);
+        System.out.println(c);
+        double d = Math.sqrt(a);
+        System.out.println(d);
+        d = Math.pow(a, 4);
+        System.out.println(d);
+        d = 5.1;
+        d = Math.ceil(d);
+        System.out.println(d);
+        d = 5.9;
+        d = Math.floor(d);
+        System.out.println(d);
+        d = Math.log(b);
+        System.out.println(d);
+        d = Math.log10(b);
+        System.out.println(d);
+        d = Math.exp(b);
+        System.out.println(d);
+        d = Math.sin(b);
+        System.out.println(d);
+        d = Math.cos(b);
+        System.out.println(d);
+        d = Math.tan(b);
+        System.out.println(d);
+
     }
 }
